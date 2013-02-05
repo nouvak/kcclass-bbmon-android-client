92558cf27a898a08d43b91585e609e9f01b67146
nouvak <nouvak@gmail.com>
2013-02-05 11:39:58 +0100

    Implemented the displayin of the observables on the main screen.

--- BBmonAndroidClient/src/si/kcclass/bbmonandroidclient/MainActivity.java
+++ BBmonAndroidClient/src/si/kcclass/bbmonandroidclient/MainActivity.java
@@ -2,31 +2,38 @@ package si.kcclass.bbmonandroidclient;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	
 	private static final String TAG = "MainActivity";
 	
-	private class DownloadObservablesList extends AsyncTask<String, Void, JSONObject> {
+	private class DownloadObservablesList extends AsyncTask<String, Void, JSONArray> {
 
 		@Override
-		protected JSONObject doInBackground(String... urls) {
+		protected JSONArray doInBackground(String... urls) {
 			return downloadServersList(urls[0]);
 		}
 		
-		private JSONObject downloadServersList(String url) {
+		private JSONArray downloadServersList(String url) {
 			try {
 				DefaultHttpClient httpClient = new DefaultHttpClient(
 						new BasicHttpParams());
@@ -35,8 +42,8 @@ public class MainActivity extends Activity {
 			    BufferedReader reader = new BufferedReader(new InputStreamReader(
 			    		httpResponse.getEntity().getContent(), "UTF-8"));
 			    String json = reader.readLine();
-			    JSONObject jsonObject = new JSONObject(json);
-			    return jsonObject;
+			    JSONArray serverList = new JSONArray(json);
+			    return serverList;
 			} catch (Exception e) {
 				Log.e(TAG, "Downloading server list failed.", e);
 				return null;
@@ -44,17 +51,33 @@ public class MainActivity extends Activity {
 		}
 		
         @Override
-        protected void onPostExecute(JSONObject result) {
+        protected void onPostExecute(JSONArray result) {
             setContentView(R.layout.activity_main);
-            // Displays the HTML string in the UI via a WebView
-            //WebView myWebView = (WebView) findViewById(R.id.webview);
-            //myWebView.loadData(result, "text/html", null);
+            // We need to use a different list item layout for devices older than Honeycomb
+            int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                    android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+            
+            List<String> observables = new ArrayList<String>();
+            for (int i=0; i<result.length(); i++) {
+				try {
+					JSONObject jsonObj;
+					jsonObj = result.getJSONObject(i);
+					observables.add(jsonObj.getString("name"));
+				} catch (JSONException e) {
+					Log.e(TAG, "Error adding json object to the observables list.", e);
+				}
+            }
+            
+            ListView observablesListView = (ListView)findViewById(R.id.observable_list);
+            observablesListView.setAdapter(new ArrayAdapter<String>(MainActivity.this, 
+            		layout, observables));
+
         }
 	}
-	
+		
     private void loadServerList() {
     	new DownloadObservablesList().execute(
-    			"http://www.hpgloe.com/json/getrec/?lat=37.234&lon=-122.234");
+    			"http://bbmonserver.cloudfoundry.com/server-instances");
     }
 
 	
