ed5fa4c9a6616d13e325d103563e984c7ae9a04c
nouvak <nouvak@gmail.com>
2013-09-08 17:13:20 +0200

    Removed the previous work.

--- BBmonAndroidClient/src/si/kcclass/bbmonandroidclient/MainActivity.java
+++ BBmonAndroidClient/src/si/kcclass/bbmonandroidclient/MainActivity.java
@@ -1,46 +1,17 @@
 package si.kcclass.bbmonandroidclient;
 
-import si.kcclass.bbmonandroidclient.fragments.ObservableDetailsFragment;
-import si.kcclass.bbmonandroidclient.fragments.ObservablesListFragment;
-import si.kcclass.bbmonandroidclient.fragments.ObservablesListFragment.OnObservableSelectedListener;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends FragmentActivity
-	implements OnObservableSelectedListener {
+public class MainActivity extends Activity {
 	
-	private static final String TAG = "MainActivity";
+	private static final String LOG_TAG = MainActivity.class.getName();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-        // Check whether the activity is using the layout version with
-        // the fragment_container FrameLayout. If so, we must add the first fragment
-        if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            // Create an instance of ExampleFragment
-            ObservablesListFragment firstFragment = new ObservablesListFragment();
-
-            // In case this activity was started with special instructions from an Intent,
-            // pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
-        }
-
 	}
 
 	@Override
@@ -50,43 +21,5 @@ public class MainActivity extends FragmentActivity
 		return true;
 	}
 	
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-	@Override
-	public void onObservableSelected(int position) {
-        // The user selected the headline of an article from the HeadlinesFragment
-
-        // Capture the article fragment from the activity layout
-        ObservableDetailsFragment observableDetailsFrag = (ObservableDetailsFragment)
-                getSupportFragmentManager().findFragmentById(R.id.observable_details_fragment);
-
-        if (observableDetailsFrag != null) {
-            // If article frag is available, we're in two-pane layout...
-
-            // Call a method in the ArticleFragment to update its content
-            observableDetailsFrag.updateObservableDetailsView(position);
-
-        } else {
-            // If the frag is not available, we're in the one-pane layout and must swap frags...
-
-            // Create fragment and give it an argument for the selected article
-            ObservableDetailsFragment newFragment = new ObservableDetailsFragment();
-            Bundle args = new Bundle();
-            args.putInt(ObservableDetailsFragment.ARG_POSITION, position);
-            newFragment.setArguments(args);
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-            // Replace whatever is in the fragment_container view with this fragment,
-            // and add the transaction to the back stack so the user can navigate back
-            transaction.replace(R.id.fragment_container, newFragment);
-            transaction.addToBackStack(null);
-
-            // Commit the transaction
-            transaction.commit();
-        }		
-	}
 
 }
